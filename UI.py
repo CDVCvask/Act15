75bdbd5bcce39023649e8cfb27d9135e9ceeccbf
CDVC <cesarcamposeco415@gmail.com>
2025-08-11 11:24:44 -0600

    Salida

---
+++ UI.py
@@ -0,0 +1,39 @@
+def Menu():
+    print("Bienvenido a Historial BingBong")
+    print("1.Agregar página al historial")
+    print("2.Ver todo el historial")
+    print("3.Buscar en el historial")
+    print("4.Eliminar elemento más antiguo del historial")
+    print("5.Salir del historial")
+allow = False
+exit = 0
+while allow == False:
+    Menu()
+    opt = input("Ingrese la opción que desee: ")
+    print(" ")
+    match opt:
+        case "1":
+            print("1")
+        case "2":
+            print("2")
+        case "3":
+            print("3")
+        case "4":
+            print("4")
+        case "5":
+            while allow == False:
+                check = input("Está seguro de que desea salir del programa? (S/N): ")
+                if check.upper() == "S":
+                    print("Gracias por utilizar el programa")
+                    exit = 1
+                    break
+                elif check.upper() == "N":
+                    print("Regresando al menú principal")
+                    input(" ")
+                    break
+                else:
+                    print("La opcción seleccionada no es valida")
+        case _:
+            print("La opción seleccionada no es valida")
+    if exit == 1:
+        break
